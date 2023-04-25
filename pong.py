7f20993aeb30c5006d2adea371455bd6b6030fad
Eshwar Muneshar <Emuneshar@gmail.com>
2023-04-25 17:44:53 -0400

    modified code

--- pong.py
+++ pong.py
@@ -5,7 +5,8 @@ window = turtle.Screen()
 window.title("Pong")
 window.bgcolor("white")
 window.setup(width=800, height = 600)
-#window.tracer(0)
+
+window.tracer(0)
 
 #while True:
 #    window.update()
