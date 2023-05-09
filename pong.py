bd43cfce96130d45d16ac3334971f09a9cee14ea
Eshwar Muneshar <Emuneshar@gmail.com>
2023-05-09 17:14:06 -0400

    Added code for score variables

--- pong.py
+++ pong.py
@@ -3,10 +3,15 @@ import os
 
 window = turtle.Screen()
 window.title("Pong")
-window.bgcolor("white")
+window.bgcolor("black")
 window.setup(width=800, height = 600)
-
 window.tracer(0)
 
-#while True:
-#    window.update()
+leftScore = 0
+rightScore = 0
+
+leftPaddle = turtle.Turtle()
+
+
+while True:
+    window.update()
